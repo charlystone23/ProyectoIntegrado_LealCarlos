5a5fe4edb85e5cdd2ca25c7c1d55de7a29ec51d7
Charly Leal <carlitos007leal@gmail.com>
2022-08-08 13:07:24 -0300

    back
    cracion de la carpeta de back

---
+++ ProyectoIntegrado/BackEnd/src/main/java/com/portfolio/charlystone/CharlystoneApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.charlystone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharlystoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharlystoneApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrado/BackEnd/src/test/java/com/portfolio/charlystone/CharlystoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.charlystone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharlystoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
